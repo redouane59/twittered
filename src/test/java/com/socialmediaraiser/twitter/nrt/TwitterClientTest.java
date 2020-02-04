b7bd78260c31c741a2959b480a02de69ae094e03
redouane59 <bali.redouane@gmail.com>
2020-02-04 16:45:01 +0100

    disabling nrt

--- src/test/java/com/socialmediaraiser/twitter/nrt/TwitterClientTest.java
+++ src/test/java/com/socialmediaraiser/twitter/nrt/TwitterClientTest.java
@@ -16,6 +16,7 @@ import java.util.Date;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 public class TwitterClientTest {
 
     private TwitterClient twitterClient = new TwitterClient();
