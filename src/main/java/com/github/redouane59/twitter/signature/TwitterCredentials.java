e49f6e3ccaaddafaef298350121b6eacc3b31544
redouane59 <bali.redouane@gmail.com>
2020-08-22 20:34:34 +0200

    feat_credentials

--- src/main/java/com/github/redouane59/twitter/signature/TwitterCredentials.java
+++ src/main/java/com/github/redouane59/twitter/signature/TwitterCredentials.java
@@ -1,14 +1,15 @@
 package com.github.redouane59.twitter.signature;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class TwitterCredentials {
     private String apiKey;
