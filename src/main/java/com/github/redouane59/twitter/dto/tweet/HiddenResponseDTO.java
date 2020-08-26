a0ccc499a0945c3353930375ed3191a6c5d72a90
redouane59 <bali.redouane@gmail.com>
2020-08-26 19:52:41 +0200

    feat_hide_reply

--- src/main/java/com/github/redouane59/twitter/dto/tweet/HiddenResponseDTO.java
+++ src/main/java/com/github/redouane59/twitter/dto/tweet/HiddenResponseDTO.java
@@ -1,5 +1,6 @@
 package com.github.redouane59.twitter.dto.tweet;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ public class HiddenResponseDTO {
   private HiddenDataDTO data;
 
   @NoArgsConstructor
+  @AllArgsConstructor
   @Setter
   @Getter
   public static class HiddenDataDTO{
