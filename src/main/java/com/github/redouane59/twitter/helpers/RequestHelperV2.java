824decde1e3870dfdd3f225fe101c6daf5b32a45
redouane59 <bali.redouane@gmail.com>
2020-09-03 13:02:07 +0200

    fix_wrong_info_log

--- src/main/java/com/github/redouane59/twitter/helpers/RequestHelperV2.java
+++ src/main/java/com/github/redouane59/twitter/helpers/RequestHelperV2.java
@@ -47,6 +47,9 @@ public class RequestHelperV2 extends AbstractRequestHelper {
       String   newUrl         = httpBuilder.build().url().toString();
       Response response       = this.getHttpClient(newUrl).newCall(request).execute();
       String   stringResponse = response.body().string();
+      if (response.code() < 200 || response.code() > 299) {
+        LOGGER.error("(POST) Error calling " + url + " " + stringResponse + " - " + response.code());
+      }
       if (response.code() == 429) {
         this.wait(stringResponse, url);
         return this.getRequestWithParameters(url, parameters, classType);
@@ -54,7 +57,6 @@ public class RequestHelperV2 extends AbstractRequestHelper {
         LOGGER.info("Error 401, user may be private");
         return Optional.empty();
       }
-      LOGGER.info(stringResponse);
       result = TwitterClient.OBJECT_MAPPER.readValue(stringResponse, classType);
     } catch (Exception e) {
       LOGGER.error(e.getMessage());
