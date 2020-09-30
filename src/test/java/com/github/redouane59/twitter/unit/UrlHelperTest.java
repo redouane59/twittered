e3e349d1b97e2bc30cc6ecb118dd50b2aa8619b3
redouane59 <bali.redouane@gmail.com>
2020-09-30 20:07:13 +0100

    feat_add_sampledstream_extension

--- src/test/java/com/github/redouane59/twitter/unit/UrlHelperTest.java
+++ src/test/java/com/github/redouane59/twitter/unit/UrlHelperTest.java
@@ -201,7 +201,9 @@ public class UrlHelperTest {
 
   @Test
   public void testSampledStreamUrl() {
-    assertEquals("https://api.twitter.com/2/tweets/sample/stream", urlHelper.getSampledStreamUrl());
+    assertEquals(
+        "https://api.twitter.com/2/tweets/sample/stream?tweet.fields=attachments,author_id,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,source,text,withheld,context_annotations,conversation_id&user.fields=id,created_at,username,name,location,url,verified,profile_image_url,public_metrics,pinned_tweet_id,description,protected",
+        urlHelper.getSampledStreamUrl());
   }
 
   @Test
