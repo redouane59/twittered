ff01568a6bcc1bcfaacb1d0f4b6df9e57be5dc2c
redouane59 <bali.redouane@gmail.com>
2020-12-19 13:03:25 +0100

    fixing test

--- src/test/java/com/github/redouane59/twitter/unit/UserFollowersDeserializerV2Test.java
+++ src/test/java/com/github/redouane59/twitter/unit/UserFollowersDeserializerV2Test.java
@@ -1,25 +1,28 @@
 package com.github.redouane59.twitter.unit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.github.redouane59.twitter.TwitterClient;
-import com.github.redouane59.twitter.dto.user.User;
-import com.github.redouane59.twitter.dto.user.UserV2;
+import com.github.redouane59.twitter.dto.user.UserListV2;
 import java.io.File;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 public class UserFollowersDeserializerV2Test {
 
-  private File userFile2 = new File(getClass().getClassLoader().getResource("tests/user_followers_example_v2.json").getFile());
-  private User userV2    = TwitterClient.OBJECT_MAPPER.readValue(userFile2, UserV2.class);
+  private File       userFile2 = new File(getClass().getClassLoader().getResource("tests/user_followers_example_v2.json").getFile());
+  private UserListV2 users     = TwitterClient.OBJECT_MAPPER.readValue(userFile2, UserListV2.class);
 
   public UserFollowersDeserializerV2Test() throws IOException {
   }
 
   @Test
   public void testGetUserIdV2() {
-    assertEquals("92073489", userV2.getId());
+    assertNotNull(users);
+    assertEquals(100, users.getData().size());
+    assertEquals("samsamia13", users.getData().get(0).getName());
+    assertEquals("606255425", users.getData().get(0).getId());
   }
 
 }
