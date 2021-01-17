0532f25992e48f4359f6ff1bae60b5084f9fade2
redouane59 <bali.redouane@gmail.com>
2021-01-17 13:02:03 +0100

    checkout last

--- src/test/java/com/github/redouane59/twitter/unit/UrlHelperTest.java
+++ src/test/java/com/github/redouane59/twitter/unit/UrlHelperTest.java
@@ -6,7 +6,6 @@ import com.github.redouane59.twitter.dto.tweet.MediaCategory;
 import com.github.redouane59.twitter.helpers.URLHelper;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -144,7 +143,7 @@ public class UrlHelperTest {
         + URLHelper.TWEET_FIELDS + URLHelper.ALL_TWEET_FIELDS
         + "&"
         + URLHelper.USER_FIELDS + URLHelper.ALL_USER_FIELDS,
-        urlHelper.getTweetListUrl(Arrays.asList("1294174710624849921,1294380029430960128,1294375095746666496")));
+        urlHelper.getTweetListUrl(List.of("1294174710624849921,1294380029430960128,1294375095746666496")));
   }
 
   @Test
