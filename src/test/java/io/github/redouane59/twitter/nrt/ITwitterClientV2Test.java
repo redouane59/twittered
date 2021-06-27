7ce1fc2819e08edd43006e557ba2e0a58e062c0c
redouane59 <bali.redouane@gmail.com>
2021-06-27 11:31:09 +0100

    cleaning

--- src/test/java/io/github/redouane59/twitter/nrt/ITwitterClientV2Test.java
+++ src/test/java/io/github/redouane59/twitter/nrt/ITwitterClientV2Test.java
@@ -446,17 +446,6 @@ public class ITwitterClientV2Test {
   }
 
   @Test
-  public void testGetLikedTweetsWithParameters() {
-    TweetList result = twitterClient.getLikedTweets(userId, AdditionalParameters.builder()
-                                                                                .recursiveCall(false).maxResults(20).build());
-    assertTrue(result.getData().size() > 0);
-    assertNotNull(result.getData().get(0).getId());
-    assertNotNull(result.getData().get(0).getText());
-    assertNotNull(result.getData().get(0).getCreatedAt());
-    assertNotNull(result.getMeta().getNextToken());
-  }
-
-  @Test
   public void testGetTweetCount() {
     TweetCountsList result = twitterClient.getTweetCounts("@Twitter");
     assertTrue(result.getData().size() > 0);
