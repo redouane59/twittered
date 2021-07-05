b697ae13760178661fde2547940d3ee78292a249
Redouane59 <bali.redouane@gmail.com>
2021-07-05 14:08:35 +0100

    feat_adding_nrt

--- src/test/java/io/github/redouane59/twitter/unit/TweetDeserializerV2Test.java
+++ src/test/java/io/github/redouane59/twitter/unit/TweetDeserializerV2Test.java
@@ -2,7 +2,9 @@ package io.github.redouane59.twitter.unit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.github.redouane59.twitter.TwitterClient;
 import io.github.redouane59.twitter.dto.tweet.ContextAnnotation;
 import io.github.redouane59.twitter.dto.tweet.ReplySettings;
@@ -125,5 +127,12 @@ public class TweetDeserializerV2Test {
     assertNotNull(tweetv2.getAttachments());
     assertNotNull(tweetv2.getAttachments().getMediaKeys());
     assertEquals("3_1365362339449561088", tweetv2.getAttachments().getMediaKeys()[0]);
+  }
+
+  @Test
+  public void testSerialization() throws JsonProcessingException {
+    String tweetAsString = TwitterClient.OBJECT_MAPPER.writeValueAsString(tweetv2);
+    assertNotNull(tweetAsString);
+    assertTrue(tweetAsString.contains("Try to use some function"));
   }
 }
