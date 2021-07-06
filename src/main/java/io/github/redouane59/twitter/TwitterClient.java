06e0f259c073747ad80ebf6f569e7ee0248ff606
Redouane Bali <bali.redouane@gmail.com>
2021-07-06 20:30:20 +0100

    fix: searchAllTweents max result management (#255)
    * init
    
    * add warn
    
    * add warn

--- src/main/java/io/github/redouane59/twitter/TwitterClient.java
+++ src/main/java/io/github/redouane59/twitter/TwitterClient.java
@@ -607,7 +607,12 @@ public class TwitterClient implements ITwitterClientV1, ITwitterClientV2, ITwitt
   public TweetList searchAllTweets(final String query, AdditionalParameters additionalParameters) {
     Map<String, String> parameters = additionalParameters.getMapFromParameters();
     parameters.put(QUERY, query);
-    parameters.put(TWEET_FIELDS, ALL_TWEET_FIELDS);
+    if (additionalParameters.getMaxResults() <= 100) {
+      parameters.put(TWEET_FIELDS, ALL_TWEET_FIELDS);
+    } else {
+      LOGGER.warn("removing context_annotations from tweet_fields because max_result is greater 100");
+      parameters.put(TWEET_FIELDS, ALL_TWEET_FIELDS.replace(",context_annotations", ""));
+    }
     parameters.put(EXPANSION, ALL_EXPANSIONS);
     String url = urlHelper.getSearchAllTweetsUrl();
     if (!additionalParameters.isRecursiveCall()) {
