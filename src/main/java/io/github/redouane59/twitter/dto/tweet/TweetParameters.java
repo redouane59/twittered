dd040009cd05ed3d4fdce3cb2bdbbe0f37ecf40f
Redouane59 <bali.redouane@gmail.com>
2021-11-04 21:41:52 +0100

    implement delete v2

--- src/main/java/io/github/redouane59/twitter/dto/tweet/TweetParameters.java
+++ src/main/java/io/github/redouane59/twitter/dto/tweet/TweetParameters.java
@@ -1,11 +1,15 @@
 package io.github.redouane59.twitter.dto.tweet;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
+@Getter
+@JsonInclude(Include.NON_NULL)
 public class TweetParameters {
 
   private String        text;
