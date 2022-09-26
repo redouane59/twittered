ec001e721e4c887fb3e738d9659ac3fe470c7ce4
MBlyzniukov <blizniukov@gmail.com>
2022-09-26 15:33:37 +0300

    Add Variant media field

--- src/main/java/io/github/redouane59/twitter/TwitterClient.java
+++ src/main/java/io/github/redouane59/twitter/TwitterClient.java
@@ -115,7 +115,7 @@ public class TwitterClient implements ITwitterClientV1, ITwitterClientV2, ITwitt
   public static final String       MEDIA_FIELD      = "media.fields";
   public static final String
                                    ALL_MEDIA_FIELDS =
-      "duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text";
+      "duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text,variants";
   public static final String       SPACE_FIELDS     = "space.fields";
   public static final String
                                    ALL_SPACE_FIELDS =
--- src/main/java/io/github/redouane59/twitter/dto/tweet/TweetV2.java
+++ src/main/java/io/github/redouane59/twitter/dto/tweet/TweetV2.java
@@ -496,6 +496,8 @@ public class TweetV2 implements Tweet {
     private String                previewImageUrl;
     @JsonProperty("public_metrics")
     private MediaPublicMetricsDTO publicMetrics;
+    @JsonProperty("variants")
+    private List<Variant>         variants;
     @JsonProperty("alt_text")
     private String                altText;
 
@@ -564,5 +566,19 @@ public class TweetV2 implements Tweet {
 
     @JsonProperty("view_count")
     private int viewCount;
+  }
+
+  @Getter
+  @Setter
+  public static class Variant {
+
+    @JsonProperty("bit_rate")
+    private int bitRate;
+
+    @JsonProperty("content_type")
+    private String contentType;
+
+    @JsonProperty("url")
+    private String url;
   }
 }
